f2be9f322fbc0cf197a1350753d9ee0acb43dab4
Travis Lang <tlang50@icloud.com>
2018-12-11 15:12:59 -0600

    add giphy api with env variable

--- server/routes/random.router.js
+++ server/routes/random.router.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 router.get('/', (req, res) => {
-    res.send('Hello World'); // Replace this
+    axios.get(`http://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_API_KEY}`)
+        .then(resp => {
+            res.send(resp.data.data);
+
+        }).catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+
+        })
 })
 
 module.exports = router;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
+import { connect } from 'react-redux';
 
 class App extends Component {
-  // Renders the entire app on the DOM
-  render() {
-    return (
-      <div>
-        <header className="App-header">
-          <h1>Random Giphy API</h1>
-        </header>
+
+    handleClick = () => {
+        axios.get('/random')
+            .then(res => {
+                console.log(res.data)
+                this.props.dispatch({ type: 'SET_RANDOM', payload: res.data })
+            }).catch(err => console.log(err))
+    }
+    render() {
+        let picture;
+        console.log(this.props.random.image_url);
         
-        <p>Results go here</p>
-      </div>
-    );
-  }
+        if (this.props.random.image_url) {
+            picture = <div><img src={this.props.random.image_url} alt='random gif' /></div>
+        }
+        else {
+            picture = <div></div>
+        }
+        return (
+            <div>
+                <header className="App-header">
+                    <h1>Random Giphy API</h1>
+                </header>
+                <div>
+                    <button onClick={this.handleClick}>Show Random GIF</button>
+                </div>
+                {picture}
+            </div>
+        );
+    }
 }
 
-export default App;
+const mapReduxStateToProps = (reduxStore) => {
+    return {
+        random: reduxStore.random
+    }
+}
+
+export default connect(mapReduxStateToProps)(App);
